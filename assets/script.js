19b8157097c3817885a5f30a555d61fec37bdd77
malek mekdashi <malekmekdashi@gmail.com>
2022-07-29 01:55:06 -0700

    more

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,4 @@
-//GIVEN I am taking a code quiz
+// GIVEN I am taking a code quiz
 // WHEN I click the start button
 // THEN a timer starts and I am presented with a question
 // WHEN I answer a question
@@ -11,10 +11,10 @@
 // THEN I can save my initials and my score
 
 var timerElement = document.querySelector(".timer-count");
-var startButton = document.querySelector(".start-button");
+var btn = document.querySelector(".btn");
 var restartButton = document.querySelector(".restart-button");
 var saveButton = document.querySelector(".save-button");
-var timer;
+var timer
 var timerCount;
 var questionCount = 0;
 
@@ -23,6 +23,65 @@ var questionCount = 0;
 //      - set the interval to decrease timer by 1sec or 1000ms
 //      - once the user clicks "start quiz", the first question should show
 //      - function to start quiz
+
+let questions = [
+    {
+        
+        question: "9 + 10 =",
+        answer: "19",
+        options: [
+            "19",
+            "21",
+            "1",
+            "100"
+        ]
+    },
+    {
+    
+        question: "(100/x) + 20 = ",
+        answer: "x = -5",
+        options: [
+            "x = 5",
+            "x = 10",
+            "x = -5",
+            "x = -10"
+        ]
+    },
+    {
+        
+        quetion: "16 - 2x = 5x + 9. solve for x",
+        answer: "x = 1",
+        options: [
+            "x = 1",
+            "x = 2",
+            "x = 3",
+            "x = 4"
+        ]
+    },
+    {
+       
+        quetion: "if m + n = -2, then the value of m^3+n^3 - 6mn ",
+        answer: "-8",
+        options: [
+            "8",
+            "4",
+            "-8",
+            "4"
+        ]
+    },
+    {
+    
+        quetion: "6 = 2(x + 2)",
+        answer: "x = 1",
+        options: [
+            "x = 4",
+            "x = 12",
+            "x = 2",
+            "x = 1"
+        ]
+    },
+];
+
 
 function startTimer() {
     timer = setInterval(function(){
@@ -38,9 +97,9 @@ function startTimer() {
 
 function startQuiz() {
     timerCount = 60;
-    startButton.disabled = true;
+    btn.disabled = true;
     startTimer();
 }
 
 
-startButton.addEventListener("click", startQuiz);
+btn.addEventListener("click", startQuiz);
