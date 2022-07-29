10b729bfc5150fb76a2c43131874db5414113904
Malek Mekdashi <malekmekdashi@maleks-air.lan>
2022-07-28 18:02:35 -0700

    first commit

---
+++ assets/questions.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+        number: 1,
+        question: "9 + 10 =",
+        answer: "19",
+        options: [
+            "19",
+            "21",
+            "1",
+            "100"
+        ]
+    },
+    {
+        number: 2,
+        question: "(100/x) + 20 = ",
+        answer: "x = -5",
+        options: [
+            "x = 5",
+            "x = 10",
+            "x = -5",
+            "x = -10"
+        ]
+    },
+    {
+        number: 3,
+        quetion: "16 - 2x = 5x + 9. solve for x",
+        answer: "x = 1",
+        options: [
+            "x = 1",
+            "x = 2",
+            "x = 3",
+            "x = 4"
+        ]
+    },
+    {
+        number: 4,
+        quetion: "if m + n = -2, then the value of m^3+n^3 - 6mn ",
+        answer: "-8",
+        options: [
+            "8",
+            "4",
+            "-8",
+            "4"
+        ]
+    },
+    {
+        number: 5,
+        quetion: "6 = 2(x + 2)",
+        answer: "x = 1",
+        options: [
+            "x = 4",
+            "x = 12",
+            "x = 2",
+            "x = 1"
+        ]
+    },
+];
--- assets/script.js
+++ assets/script.js
@@ -9,3 +9,38 @@
 // THEN the game is over
 // WHEN the game is over
 // THEN I can save my initials and my score
+
+var timerElement = document.querySelector(".timer-count");
+var startButton = document.querySelector(".start-button");
+var restartButton = document.querySelector(".restart-button");
+var saveButton = document.querySelector(".save-button");
+var timer;
+var timerCount;
+var questionCount = 0;
+
+// - Need timer to start when clicking "start quiz"
+//      - var for timer
+//      - set the interval to decrease timer by 1sec or 1000ms
+//      - once the user clicks "start quiz", the first question should show
+//      - function to start quiz
+
+function startTimer() {
+    timer = setInterval(function(){
+        timerCount--;
+        timerElement.textContent = timerCount
+        if (timerCount <= 0 || questionCount === questions.length) {
+            clearInterval(timer);
+        }
+    }, 1000)
+}
+
+
+
+function startQuiz() {
+    timerCount = 60;
+    startButton.disabled = true;
+    startTimer();
+}
+
+
+startButton.addEventListener("click", startQuiz);
